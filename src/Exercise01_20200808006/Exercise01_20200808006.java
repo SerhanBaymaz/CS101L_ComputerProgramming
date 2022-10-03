a584218d0fdb4481ad5e14b5213f9d845013e057
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2022-10-03 23:22:34 +0300

    Third question added.

--- src/Exercise01_20200808006/Exercise01_20200808006.java
+++ src/Exercise01_20200808006/Exercise01_20200808006.java
@@ -30,6 +30,32 @@ public class Exercise01_20200808006 {
 
 
 
+           /*
+           3. π can be computed using the following formula:
+                𝜋 = 4 𝑥 (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 ...)
+
+                Write a program that displays the result of ;
+                4 𝑥 (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11)  and 4 𝑥 (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + 1/13)
+            */
+            double afterThe4TimesPart1 = 0;
+            double n = 1;
+        for (int i = 1; i <4 ; i++) {
+            afterThe4TimesPart1 = afterThe4TimesPart1 + (2/(n*(n+2)));
+                n = n+4;
+        }
+        System.out.println(4*afterThe4TimesPart1);
+
+
+        double afterThe4TimesPart2 = 0;
+        double m = 1;
+        for (int i = 1; i <5 ; i++) {
+            afterThe4TimesPart2 = afterThe4TimesPart2 + (2/(m*(m+2)));
+            m = m+4;
+        }
+        System.out.println(4*afterThe4TimesPart2);
+
+
+
 
     }
 }

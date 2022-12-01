a266eef3f98bd74be676358a35ef0706a7ef5a63
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2022-12-01 17:47:38 +0300

    Fourth Question implemented.

--- src/Week8_101L/Week8_101L.java
+++ src/Week8_101L/Week8_101L.java
@@ -38,13 +38,35 @@ public class Week8_101L {
     }
 
 
+    //4. Write a method that finds the occurrences of each value in a given array.
+    public static void findOccurences(int[] arr){
+        int count=0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int lookigThisNumber = arr[i];
+
+            if (arr[i]!=-999){
+
+                for (int j = 0; j <arr.length ; j++) {
+                    if (lookigThisNumber==arr[j]){
+                        count++;
+                        arr[j]=-999;
+                    }
+                }
+
+                System.out.println("The number "+lookigThisNumber+" is repeated "+count+" times");
+                count = 0;
+
+            }
+        }
+    }
 
 
 
     public static void main(String[] args) {
 
         //Q1
-        int[] myArray = {1,8,9,6,2,3,5,4,7,8,6,8,8,9};
+        int[] myArray = {1,2,2,3,3,3,4,4,4,4,9,9,9,14,12,36,96,14,12,14};
         System.out.print("Your array is : ");
         displayArr(myArray);
 
@@ -54,6 +76,10 @@ public class Week8_101L {
 
         //Q3
         System.out.println("Key in the arrays' index is : "+findElement(myArray,5));
+
+        //Q4
+        findOccurences(myArray);
+
 
 
 

42a4dc4fa67d124d0c36d62f81f0d4d537c8d1a3
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2022-12-02 13:45:21 +0300

    First Question implemented.

--- src/Week9_101L/Week9_101L.java
+++ src/Week9_101L/Week9_101L.java
@@ -26,6 +26,26 @@ public class Week9_101L {
     }
 
 
+    //1. Write a method that returns the sum of all the elements in a specified
+    //a. Column
+    //b. Row
+    public static int sumColumn(int[][] matrix, int columnIndex){
+        int sum = 0;
+        for (int i = 0; i <matrix.length ; i++) {
+            sum=sum+matrix[i][columnIndex];
+        }
+
+        return  sum;
+    }
+    public static int sumRow(int[][] matrix, int rowIndex){
+        int sum = 0;
+        for (int i = 0; i < matrix[0].length; i++) {
+            sum=sum+matrix[rowIndex][i];
+        }
+
+        return sum;
+    }
+
 
 
 
@@ -35,13 +55,19 @@ public class Week9_101L {
     public static void main(String[] args) {
 
         int[] array1D = {5,6,7,8,9,10,11,19,85,196};
-        int[][] array2D = {{45,12,69},{14,56,48},{32,69,42}};
+        int[][] array2D = {{45,12,69},{14,56,48},{32,69,42},{10,11,12}};
 
 
         //Q0
         display1DimensionArray(array1D);
         display2DimensionArray(array2D);
         System.out.println("----------------------------------------------");
+
+        //Q1
+        System.out.println("Sum of the column is : " + sumColumn(array2D,0));
+        System.out.println("Sum of the row is : " + sumRow(array2D,1));
+        System.out.println("----------------------------------------------");
+
 
 
 
